bf20d8fdd72fb8220918e36d4b807dd833263aa7
Adilson Junior <adilsonagj@unipam.edu.br>
2019-04-25 12:34:59 -0300

    my project

---
+++ src/app/oficina1/oficina1.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-oficina1',
+  templateUrl: './oficina1.page.html',
+  styleUrls: ['./oficina1.page.scss'],
+})
+export class Oficina1Page implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
