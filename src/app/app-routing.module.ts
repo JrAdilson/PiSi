9d6c6c4e85a6b5d5e0b8258ea229163f59b6091f
Samuel Silca <Samuel Ximenes>
2019-06-28 14:30:21 -0300

    page login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' },
-  { path: 'home', loadChildren: './home/home.HomePageModule' },
+  { path: 'home', loadChildren: './tab1/tab1.Tab1PageModule' },
   { path: 'oficina1', loadChildren: './oficina1/oficina1.module#Oficina1PageModule' },
   { path: 'oficina2', loadChildren: './oficina2/oficina2.module#Oficina2PageModule' },
-  { path: 'oficina3', loadChildren: './oficina3/oficina3.module#Oficina3PageModule' },  { path: 'conta', loadChildren: './conta/conta.module#ContaPageModule' },
+  { path: 'oficina3', loadChildren: './oficina3/oficina3.module#Oficina3PageModule' },
+  { path: 'conta', loadChildren: './conta/conta.module#ContaPageModule' },  { path: 'login', loadChildren: './login/login.module#LoginPageModule' },
+
 
   
   
